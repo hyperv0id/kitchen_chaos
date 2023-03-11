51fb37bd603cc205ebebd0472987abdf6aa6c664
jcheng <20722003@bjtu.edu.cn>
2023-03-12 01:37:44 +0800

    new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private GameInput gameInput;
     private bool isWalking;
 
     void Update() {
-        Vector2 inputVector = new();
-        if (Input.GetKey(KeyCode.W)){
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)){
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)){
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)){
-            inputVector.x = +1;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime);
